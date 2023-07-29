9c5feac972e2372d325569262a376fe5de189f30
Utkarsh <utalmighty@gmail.com>
2023-07-29 14:51:10 +0530

    Catch block

--- logic.js
+++ logic.js
@@ -20,7 +20,7 @@ function checkRequiredFields() {
 }
 
 function checkEmailPattern(emailValue) {
-    emailReg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const emailReg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailReg.test(emailValue);
 }
 
@@ -39,9 +39,8 @@ function post() {
         headers: {
             "Content-type": "application/json; charset=UTF-8"
         }
-    }).then(resp => {
-        processResponse(resp);
-    })
+    }).then(resp => processResponse(resp))
+    .catch (error => alert("Something went wrong!"));
 }
 
 function processResponse(resp) {
@@ -73,7 +72,7 @@ function prepareDate() {
 
 function checkDate(day, month, year) {
     let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    var today = new Date();
+    const today = new Date();
     if (year < 1900 || year > today.getFullYear()) return false;
     if (month < 1 && month > 12) return false;
     if (day > 0) {
@@ -87,7 +86,7 @@ function checkDate(day, month, year) {
 function  isLeapYear(year) {
     if ((year % 400 == 0) && (year % 100 == 0))
         return true;
-    if ((year % 4 ==0) && (year % 100 != 0))
+    if ((year % 4 == 0) && (year % 100 != 0))
         return true;
     return false;
 }
